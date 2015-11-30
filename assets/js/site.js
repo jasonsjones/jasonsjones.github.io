37b84cd078359f7f15826cd2223412b71bb44ed2
Jason Jones <jsjones96@gmail.com>
2015-11-30 23:10:05 +0000

    Remove slide in animation

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,13 +1,9 @@
 $(document).ready(function () {
     // code will go here...
-    if (window.location.href === "http://localhost:4000/" ||
-        window.location.href === "https://jasonsjones.github.io/") {
-        $('.header-content').addClass('animated fadeInLeft');
-    }
 
     $('.down-button').addClass('animated shake');
 
     $('.down-button').click(function () {
         $(this).fadeOut('slow');
-    })
+    });
 });
