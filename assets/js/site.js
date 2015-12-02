302c9a9c8be0c5956d350b6e2812b1b0b9278e4e
Jason S. Jones <jsjones96@gmail.com>
2015-12-02 06:25:00 -0800

    Add smooth scolling to #next-section

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
     $('.down-button').addClass('animated shake');
 
     $('.down-button').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#next-section').offset().top-50
+        }, 1500);
         $(this).fadeOut('slow');
     });
 });
